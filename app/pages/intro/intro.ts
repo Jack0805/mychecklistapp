e764e73ed10887afd102f5f375c6efe21a57ff2f
jack <zhangke0805@gmail.com>
2016-08-19 11:53:24 +1000

    the initial upload

---
+++ app/pages/intro/intro.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {HomePage} from '../home/home';
+
+@Component({
+  templateUrl: 'build/pages/intro/intro.html'
+})
+export class IntroPage {
+
+	slideOptions: any;	
+
+	constructor(public nav: NavController){
+		this.slideOptions = {
+			pager: true
+		};
+	}
+
+	goToHome(): void {
+	this.nav.setRoot(HomePage);
+	}
+}
